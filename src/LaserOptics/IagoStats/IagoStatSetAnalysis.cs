61d4324a82c9f6739b1c0e8f545d1bced2546305
wilsohn <Wilson.Tzou@iqmetrix.com>
2014-03-14 16:15:16 -0700

    Pulling over IagoStats files and stats interfaces from oxyplot-refactor branch

---
+++ src/LaserOptics/IagoStats/IagoStatSetAnalysis.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using LaserOptics.Common;
+
+namespace LaserOptics.IagoStats
+{
+    public class IagoStatSetAnalysis
+    {
+        private readonly List<String> _requestLatencySeries;
+        
+        public IagoStatSetAnalysis()
+        {
+            // ToDo: pull from object config 
+            _requestLatencySeries = new List<string>
+                {
+                    "client/request_latency_ms_average",
+                    "client/request_latency_ms_maximum",
+                    "client/request_latency_ms_minimum",
+                    //"client/request_latency_ms_p50",
+                    //"client/request_latency_ms_p90",
+                    //"client/request_latency_ms_p95"
+                };
+        }
+
+        public void IagoRequestLatencySummary(IStatisticsSetAnalysis statSet)
+        {
+            var averages = statSet.AnalysisScratchPad.Averages;
+
+            // Latency summary
+
+            var seriesData = new List<SeriesData>();
+            var notes = new StringBuilder();
+
+            // get the stat-by-stat values
+            foreach (var seriesName in _requestLatencySeries)
+            {
+                var data = new List<double>();
+                foreach (var stats in statSet.Statistics)
+                {
+                    if (stats.ContainsKey(seriesName))
+                    {
+                        data.Add(stats[seriesName]);
+                    }
+                    else
+                    {
+                        data.Add(data.Count > 0 ? data[data.Count - 1] : 0d);
+                        notes.AppendFormat("{0} Missing value for {1}\n", stats.TimeStamp, seriesName);
+                    }
+                }
+
+                seriesData.Add(new SeriesData(seriesName, data));
+            }
+
+            // add the averages from the entire dataset for each value
+            var avgRequestLatencySeries = new List<string>();
+            foreach (var series in _requestLatencySeries)
+            {
+                var avgSeriesName = series + " set average";
+                avgRequestLatencySeries.Add(avgSeriesName);
+                var data = new List<double>();
+                double avg = averages[series];
+                for (int i = 0; i < statSet.Statistics.Count; i++)
+                {
+                    data.Add(avg);
+                }
+
+                seriesData.Add(new SeriesData(avgSeriesName, data));
+            }
+
+            var labels =
+                statSet.Statistics.Select(stats => stats.TimeStamp.ToString(CultureInfo.InvariantCulture)).ToList();
+
+            var requestLatencyGraphs = new List<GraphData>();
+            var requestLatencyGraph = new GraphData("Request Latencies", labels, true, LegendPositionEnum.Footer, false,
+                                                    GraphType.Line, seriesData);
+            requestLatencyGraphs.Add(requestLatencyGraph);
+
+            var analysisSummary = new StringBuilder(
+                @"# Average Request Latency
+
+The following graphs the average client request statistics for each minute of the test duration.
+"
+                );
+
+            if (notes.Length > 0)
+            {
+                analysisSummary.Append(
+                    @"*Notes*:
+Missing data is replaced either with a 0 when there is no preceeding data or the previous value.
+
+*Time stamps with Missing Data*
+"
+                    );
+                analysisSummary.Append(notes);
+
+            }
+
+            // Create Detailed graphs for each Client Request Latency
+            foreach (var latencySeries in _requestLatencySeries)
+            {
+                seriesData = new List<SeriesData>();
+                var data = new List<double>();
+
+                foreach (var stats in statSet.Statistics)
+                {
+                    if (stats.ContainsKey(latencySeries))
+                    {
+                        data.Add(stats[latencySeries]);
+                    }
+                    else
+                    {
+                        data.Add(data.Count > 0 ? data[data.Count - 1] : 0d); 
+                        // don't need to add anything to the notes; would've alread been done when we generated the combined log graph above
+                    }
+                }
+                seriesData.Add(new SeriesData(latencySeries, data));
+
+                requestLatencyGraph = new GraphData(latencySeries, labels, true, LegendPositionEnum.Footer, false,
+                                          GraphType.Line, seriesData);
+                requestLatencyGraphs.Add(requestLatencyGraph);
+            }
+
+            statSet.AddAnalysisNote(new AnalysisNote("Request Latencies", analysisSummary.ToString(),
+                                                     requestLatencyGraphs));
+        }
+    }
+}
--- src/LaserOptics/IagoStats/IagoStatisticsSet.cs
+++ src/LaserOptics/IagoStats/IagoStatisticsSet.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Dynamic;
@@ -11,51 +12,67 @@ namespace LaserOptics.IagoStats
 {
     public class IagoStatisticsSet : IStatisticsSet, IStatisticsSetAnalysis
     {
-        private readonly IDictionary<string, object> _configuration;
-
+        private readonly string _inputFile;
         private readonly dynamic _analysisScratchPad = new ExpandoObject();
         private readonly List<IagoStatistics> _iagoStatistics = new List<IagoStatistics>();
-        private ReadOnlyCollection<IStatisticsValues> _roIagoStatistics ;
         private readonly List<AnalysisNote> _analysisNotes = new List<AnalysisNote>();
         private ReadOnlyCollection<AnalysisNote> _roAnalysisNote;
-
-        public IagoStatisticsSet()
-        {
-            _configuration = YamlParser.GetConfiguration();
-        }
-
-        public ReadOnlyCollection<IStatisticsValues> Statistics 
-        {
-            get { return _roIagoStatistics ?? ( _roIagoStatistics = new ReadOnlyCollection<IStatisticsValues>(_iagoStatistics.Cast<IStatisticsValues>().ToList())); }
-        }
 
         public ReadOnlyCollection<AnalysisNote> AnalysisNotes
         {
             get { return _roAnalysisNote ?? ( _roAnalysisNote = new ReadOnlyCollection<AnalysisNote>(_analysisNotes)); }
         }
 
-        public ReadOnlyCollection<ReadOnlyDictionary<string, double>> ExportStatistics(bool firstRowHeaders = true, string defValue = "missing")
+        public DateTime ActualStartTime { get; private set; }
+        public DateTime ActualEndTime { get; private set; }
+        public DateTime DesiredStartTime { get; set; }
+        public DateTime DesiredEndTime { get; set; }
+
+        public dynamic AnalysisScratchPad { get { return _analysisScratchPad; } }
+
+        public IagoStatisticsSet()
+        {
+            Dictionary<object, object> configuration = YamlParser.GetConfiguration();
+
+            if (configuration.ContainsKey("FieldsToIgnore"))
+            {
+                AnalysisScratchPad.ignorableFields = configuration["FieldsToIgnore"];
+            }
+            else
+            {
+                AnalysisScratchPad.ignorableFields = new List<string>();
+            }
+
+            _inputFile = configuration["InputFile"].ToString();
+        }
+
+        public void Parse()
+        {
+            using (var input = File.OpenText(_inputFile))
+            {
+                string line;
+                while ((line = input.ReadLine()) != null)
+                {
+                    var newStat = new IagoStatistics();
+                    newStat.Parse(line);
+                    _iagoStatistics.Add(newStat);
+                }
+            }
+        }
+
+        public void Parse(DateTime startTime, DateTime endTime)
         {
             throw new NotImplementedException();
         }
 
-        public dynamic AnalysisScratchPad { get { return _analysisScratchPad; } }
-
-        public void Parse()
+        public IStatisticsValues GetNearest(DateTime time)
         {
-            //Cv.KMeans2(points, maxClusters, clusters, new CvTermCriteria(10, 1.0));
+            throw new NotImplementedException();
+        }
 
-            StreamReader input = File.OpenText(_configuration["InputFileName"].ToString());
-
-            string line;
-            while ((line = input.ReadLine()) != null)
-            {
-                var newStat = new IagoStatistics();
-                newStat.Parse(line);
-                _iagoStatistics.Add(newStat);
-            }
-
-            input.Close();
+        public ReadOnlyCollection<ReadOnlyDictionary<string, double>> ExportStatistics(bool firstRowHeaders = true, string defValue = "missing")
+        {
+            throw new NotImplementedException();
         }
 
         public void Analyze(IEnumerable<SetAnalyzer> setAnalyzers , IEnumerable<StatAnalyzer> statAnalyzers)
@@ -79,6 +96,63 @@ namespace LaserOptics.IagoStats
             _analysisNotes.Add(note);
         }
 
+        public IEnumerator<IStatisticsValues> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public void Add(IStatisticsValues item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Clear()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Contains(IStatisticsValues item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CopyTo(IStatisticsValues[] array, int arrayIndex)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Remove(IStatisticsValues item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Count { get; private set; }
+        public bool IsReadOnly { get; private set; }
+        public int IndexOf(IStatisticsValues item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(int index, IStatisticsValues item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveAt(int index)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IStatisticsValues this[int index]
+        {
+            get { throw new NotImplementedException(); }
+            set { throw new NotImplementedException(); }
+        }
     }
 
 }
