336e07d6af9cdffc61420845ea97d80f46920561
wilsohn <Wilson.Tzou@iqmetrix.com>
2014-03-21 15:00:54 -0700

    A bit of refacotring on the FtpClient

--- src/NorwegianBlue.Util.Web/FtpClient.cs
+++ src/NorwegianBlue.Util.Web/FtpClient.cs
@@ -24,7 +24,25 @@ namespace NorwegianBlue.Util.Web
             _serverUri = serverUri;
         }
 
-        public List<FileInfo> DownloadFiles()
+        //public List<FileInfo> DownloadLogFiles()
+        //{
+        //    var localSaveFiles = new List<FileInfo>();
+
+        //    using (_ftpClient)
+        //    {
+        //        var files = _ftpClient.GetListing(_ftpClient.GetWorkingDirectory(), FtpListOption.AllFiles);
+
+        //        localSaveFiles.AddRange(from file in files
+        //                                where file.Type == FtpFileSystemObjectType.File
+        //                                select SaveToTemporaryFile(file));
+
+        //        _ftpClient.Disconnect();
+        //    }
+
+        //    return localSaveFiles;
+        //}
+
+        public List<FileInfo> PullLogFiles()
         {
             //if (_serverUri.Scheme != Uri.UriSchemeFtp)
             //{
@@ -39,7 +57,7 @@ namespace NorwegianBlue.Util.Web
 
                 localSaveFiles.AddRange(from file in files
                                     where file.Type == FtpFileSystemObjectType.File
-                                    select SaveToFile(file));
+                                    select SaveToTemporaryFile(file));
 
                 _ftpClient.Disconnect();
             }
@@ -71,13 +89,27 @@ namespace NorwegianBlue.Util.Web
             }
         }
 
-        private FileInfo SaveToFile(FtpListItem ftpListItem)
+        //private FileInfo SaveToFile(FtpListItem ftpListItem)
+        //{
+        //    var tmp = Path.GetTempFileName(); // create a concrete file instead
+        //    WriteToFile(ftpListItem.FullName, tmp);
+
+        //    return new FileInfo(tmp);
+        //}
+
+        private FileInfo SaveToTemporaryFile(FtpListItem ftpListItem)
         {
             var tmp = Path.GetTempFileName();
+            WriteToFile(ftpListItem.FullName, tmp);
 
-            using (Stream ftpStream = _ftpClient.OpenRead(ftpListItem.FullName))
+            return new FileInfo(tmp);
+        }
+
+        private void WriteToFile(string src, string dest)
+        {
+            using (Stream ftpStream = _ftpClient.OpenRead(src))
             {
-                using (FileStream localStream = File.OpenWrite(tmp))
+                using (FileStream localStream = File.OpenWrite(dest))
                 {
                     var bytesInStream = new byte[ftpStream.Length];
                     ftpStream.Read(bytesInStream, 0, bytesInStream.Length);
@@ -87,8 +119,6 @@ namespace NorwegianBlue.Util.Web
 
                 ftpStream.Close();
             }
-
-            return new FileInfo(tmp);
         }
     }
 }
